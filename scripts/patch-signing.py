3063aa2a1cf1cda3d9da0e057e4ae71779ce9677
Brad Thurston <brad.thurston@gmail.com>
2026-07-12 20:32:21 -0500

    Fix: set CODE_SIGN_IDENTITY to iPhone Distribution for Release builds

--- scripts/patch-signing.py
+++ scripts/patch-signing.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-"""Modify Xcode project.pbxproj to add code signing settings."""
+"""Modify Xcode project.pbxproj to add code signing settings for distribution."""
 import sys, re
 
 pbxproj = sys.argv[1]
@@ -12,17 +12,23 @@ with open(pbxproj, 'r') as f:
 content = re.sub(r'CODE_SIGN_STYLE = [^;]+;', 'CODE_SIGN_STYLE = Manual;', content)
 
 # Add DEVELOPMENT_TEAM after each ASSETCATALOG_COMPILER_APPICON_NAME line
-# (which appears once per build config in the target section)
 content = re.sub(
     r'(ASSETCATALOG_COMPILER_APPICON_NAME = AppIcon;)',
     r'\1\n\t\t\t\tDEVELOPMENT_TEAM = DN52659LL2;',
     content
 )
 
-# Add PROVISIONING_PROFILE_SPECIFIER 
+# Add PROVISIONING_PROFILE_SPECIFIER
 content = re.sub(
     r'(DEVELOPMENT_TEAM = DN52659LL2;)',
     r'\1\n\t\t\t\tPROVISIONING_PROFILE_SPECIFIER = "' + profile_name + '";',
+    content
+)
+
+# Set CODE_SIGN_IDENTITY to iPhone Distribution for Release configs
+content = re.sub(
+    r'/\* Release \*/(.*?)CODE_SIGN_IDENTITY = "iPhone Developer"',
+    r'/* Release */\1CODE_SIGN_IDENTITY = "iPhone Distribution"',
     content
 )
 
